245d737048c7b88c8103ea7e3a171f08c948a6b3
Ranveer Singh <rsingh.ala03@gmail.com>
2025-03-28 18:17:48 -0400

    Initial Commit

---
+++ FridgeAIApp/FridgeAIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeAIAppApp.swift
+//  FridgeAIApp
+//
+//  Created by Ranveer Singh on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct FridgeAIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FridgeAIAppTests/FridgeAIAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FridgeAIAppTests.swift
+//  FridgeAIAppTests
+//
+//  Created by Ranveer Singh on 3/28/25.
+//
+
+import Testing
+@testable import FridgeAIApp
+
+struct FridgeAIAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
